46dff1d83a4a19afce667421201831f7980700d3
Edward <asantanadurango@gmail.com>
2022-06-17 12:06:54 -0500

    Se agrega codigo de arreglos

--- src/js/controladorIndex.js
+++ src/js/controladorIndex.js
@@ -1,9 +1,47 @@
-//ENTRADAS == VARIABLES
-var nombre="JAMES RODRIGUEZ"
 
-if(true){
-    var nombre="Lucho diaz"
+//construyendo arreglos
+
+// 1. FORMA TRADICIONAL
+let numeros = [1, 2, 3, 4, 5]
+let nombres = ['juan', 'camila', 'andres', 'catalina'] 
+let votos = [true, true, false, false, false, true]
+
+// 2. FORMA POO
+let numerosPOO = Array (1, 2, 3, 4, 5)
+let nombresPOO = Array ('juan', 'camila', 'andres' , 'catalina')
+let votosPOO = Array (true, true, false, false, false, true)
+
+//IMPRIMIR LOS ELEMENTOS DEL ARREGLO
+console.log(numeros)
+console.log(numerosPOO)
+
+//ACCEDER A UN ELEMENTO DEL ARREGLO
+console.log(numeros[3])
+console.log(numeros[4])
+
+//OBJETOS EN JS -> atributos
+let persona = {
+    nombre : 'juan',
+    edad : 34,
+    cedula : '1234261',
+    hinchadelNacional : true,
+    foto : 'url'
+
 }
-console.log(`hola ${nombre}`)
-console.log("hola")
-console.log("adios")
+
+console.log (persona)
+console.log(persona.hinchadelNacional)
+
+
+let producto = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+console.log(producto.precio)
+
+// JSON = Java script object notation, para intercambiar datos 
